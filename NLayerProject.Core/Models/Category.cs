f0808e1bbfb7ee72143bee843cd71e51936ff2f3
muammer03karaca <muammer03karaca@gmail.com>
2021-09-09 17:36:00 +0300

    Add project files.

---
+++ NLayerProject.Core/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ NLayerProject.Core/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int  Stock { get; set; }
+        public decimal Price { get; set; }
+        public string CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+        public Category Category { get; set; }
+    }
+}
