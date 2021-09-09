411851f10365a0a0d428c280cd3ca6e6dcba516e
muammer03karaca <muammer03karaca@gmail.com>
2021-09-09 18:06:27 +0300

    3)

---
+++ NLayerProject.Core/Serivces/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Serivces
+{
+    public interface IService<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync(int id);
+        Task<IQueryable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+        Task AddASync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        TEntity Update(TEntity entity);
+    }
+}
